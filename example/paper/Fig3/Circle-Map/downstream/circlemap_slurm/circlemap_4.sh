d3a3f6ea07af470e1b41493e2013f33598a9e5ce
ustc_fmh <50505661+ustc-fmh@users.noreply.github.com>
2023-12-22 10:27:17 +0800

    Add files via upload

---
+++ example/paper/Fig3/Circle-Map/downstream/circlemap_slurm/circlemap_4.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#An example for serial job.
+#SBATCH -J circlemap_4
+#SBATCH -o log/circlemap_4_%j.log
+#SBATCH -e log/circlemap_4_%j.err
+#SBATCH --time=7-00
+#SBATCH -N 2 -n 80 
+#SBATCH -p CPU-Small
+#SBATCH --qos scpujoblimit
+#module load intel/2020
+echo Running on $SBATCH_PARTITION paratation
+echo Time is `date`
+source /home/lifesci/luosongwen/miniconda3/etc/profile.d/conda.sh
+conda activate ecc_pipe_old
+echo Directory is $PWD
+echo This job runs on the following nodes:
+echo $SLURM_JOB_NODELIST
+echo This job has allocated $SLURM_JOB_CPUS_PER_NODE cpu core.
+
+cd /home/lifesci/liuk0617/workspace/ecc_pipe/ecc_pipe
+python3 ecc_pipe_master.py --Detect --tool circlemap -n 80 --config /home/lifesci/liuk0617/workspace/ecc_pipe/ecc_pipe/config/circlemap/circlemap_4.yaml
